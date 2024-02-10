c9b2d9bd1ea1298e4020fda69641615aaa03e4ca
Mohan Ch <mail2mohan.ch@gmail.com>
2024-02-10 09:25:54 +0530

    Update electricity_bill.sol

--- electricity_bill.sol
+++ electricity_bill.sol
@@ -1,1 +1,17 @@
-
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract E_Bill{
+    string consumerName;
+    address consumerAddress;
+    uint256 consumerId;
+    uint256 meterNumber;
+    uint256 prev_reading=5;
+    uint256 curr_reading=10;
+    function checkNum(uint256 _curr_reading,uint256 _prev_reading) public pure returns (string memory) {
+        if (_prev_reading <= _curr_reading) {
+            return "Valid Input";
+        }  else {
+            return "Check your current reading always > previous reading";
+        }
+    }
+}
